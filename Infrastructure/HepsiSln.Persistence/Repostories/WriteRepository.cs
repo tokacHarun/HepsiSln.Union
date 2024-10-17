b5e6c4f40507be05be04bcc623715bbdff5c555e
tokacharun5151 <tokacharun5151@gmail.com>
2024-10-17 23:09:53 +0300

    Write Repositoryler Yapıldı

--- Infrastructure/HepsiSln.Persistence/Repostories/WriteRepository.cs
+++ Infrastructure/HepsiSln.Persistence/Repostories/WriteRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HepsiSln.Application.Interfaces.Repostories;
+using HepsiSln.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,35 @@ using System.Threading.Tasks;
 
 namespace HepsiSln.Persistence.Repostories
 {
-    public class WriteRepository
+    public class WriteRepository<T> : IWriteRepository<T> where T : class, IBaseEntity, new()
     {
+        private readonly DbContext dbContext;
+        public WriteRepository(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        private DbSet<T> Table { get => dbContext.Set<T>(); }
+
+        public async Task AddAsync(T entity)
+        {
+            await Table.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IList<T> entities)
+        {
+            await Table.AddRangeAsync(entities);
+        }
+
+        public async Task HardDeleteAsync(T entity)
+        {
+            await Task.Run(() => Table.Remove(entity));
+        }
+
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            await Task.Run(() => Table.Update(entity));
+            return entity;
+        }
     }
 }
