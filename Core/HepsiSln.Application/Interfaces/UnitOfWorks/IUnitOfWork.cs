850ba9c494d9011e8d266ea4544dde3a838e522a
tokacharun5151 <tokacharun5151@gmail.com>
2024-10-20 21:54:31 +0300

    UnitOfWork Yapısı Kullanıldı

---
+++ Core/HepsiSln.Application/Interfaces/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using HepsiSln.Application.Interfaces.Repostories;
+using HepsiSln.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiSln.Application.Interfaces.UnitOfWorks
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IReadRepository<T> GetReadRepository<T>() where T : class, IBaseEntity, new();
+        IWriteRepository<T> GetWriteRepository<T>() where T : class, IBaseEntity, new();
+        Task<int> SaveAsync();
+        int Save();
+    }
+}
---
+++ Infrastructure/HepsiSln.Persistence/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using HepsiSln.Application.Interfaces.Repostories;
+using HepsiSln.Application.Interfaces.UnitOfWorks;
+using HepsiSln.Persistence.Context;
+using HepsiSln.Persistence.Repostories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiSln.Persistence.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext dbContext;
+
+        public UnitOfWork(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async ValueTask DisposeAsync() => await dbContext.DisposeAsync();
+
+
+        public int Save() => dbContext.SaveChanges();
+
+
+        public async Task<int> SaveAsync() => await dbContext.SaveChangesAsync();
+
+
+
+        IReadRepository<T> IUnitOfWork.GetReadRepository<T>() => new ReadRepository<T>(dbContext);
+
+
+        IWriteRepository<T> IUnitOfWork.GetWriteRepository<T>() => new WriteRepository<T>(dbContext);
+       
+    }
+}
