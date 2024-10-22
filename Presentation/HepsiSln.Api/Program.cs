79656e79a214d94a963468e7391f4c4470920607
tokacharun5151 <tokacharun5151@gmail.com>
2024-10-22 22:58:43 +0300

    MediatR kullanıldı

--- Presentation/HepsiSln.Api/Program.cs
+++ Presentation/HepsiSln.Api/Program.cs
@@ -1,4 +1,5 @@
 using HepsiSln.Persistence;
+using HepsiSln.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +19,7 @@ builder.Configuration
     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
 
 builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
